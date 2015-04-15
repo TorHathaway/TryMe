18f6606dcf039427e8c335c991f7360039867763
Tor Hathaway <torhathaway@ip0af54a3c.int.uvm.edu>
2015-04-15 14:08:44 -0400

    First Commit

---
+++ header.php
@@ -0,0 +1,8 @@
+
+
+<!-- ######################     Page header   ############################## -->
+<header>
+    <h1>Sign up for my Mailing List</h1>
+</header>
+
+
---
+++ index.php
@@ -0,0 +1,28 @@
+
+
+<?php
+include "top.php";
+?>
+    <article id="main">
+        <h1>Home</h1>
+        
+        <p>Here, I hope to gather follower for my soundcloud who would like to be updated on my latest tracks. This form would allow me to gather a database of people who like my music. I would then be able to send them information on my gigs, and new tracks I put out. </p>
+        <figure>
+            <img src = "images/TorHeadshot.jpg" alt= "My face" width= "300">
+            <figcaption> Here is a picture of me. </figcaption>
+        </figure>
+        <br>
+            
+            
+       </article>
+
+    <aside id="other">
+    <h2>More About me</h2>
+    
+    <p>Here will be some extra info on who I am as a musician and my future plans. Including up coming shows, my latest mixes and mashups, artists I believe are on the up and rising. Stay tuned to keep the beats bumping all night</p>
+    </aside>
+
+<?php include "footer.php"; ?>
+
+</body>
+</html>
